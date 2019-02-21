b52094e0e5862df37bb8475506b8edfc5c0b668a
Jamie Sherman <jamie.sherman@gmail.com>
2019-02-20 17:24:16 -0800

    most of the components are present but still need to be wired together properly

--- sarcomereannotation/interface.py
+++ sarcomereannotation/interface.py
@@ -33,7 +33,12 @@ from kivy.app import App
 from kivy.logger import Logger
 from kivy.uix.scatter import Scatter
 from kivy.properties import StringProperty
+from kivy.uix.floatlayout import FloatLayout
+from kivy.factory import Factory
+from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
 
+import os
 
 class Picture(Scatter):
     '''Picture is the class that will show the image with a white border and a
@@ -49,25 +54,85 @@ class Picture(Scatter):
 
 class PicturesApp(App):
 
+    def build_config(self, config):
+        pass
+
     def build(self):
 
         # the root is created in pictures.kv
         root = self.root
 
         # get any files into images directory
-        curdir = dirname(__file__)
-        for filename in glob(join(curdir, 'images', '*')):
-            try:
-                # load the image
-                picture = Picture(source=filename, rotation=randint(-30, 30))
-                # add to the main field
-                root.add_widget(picture)
-            except Exception as e:
-                Logger.exception('Pictures: Unable to load <%s>' % filename)
+        # curdir = dirname(__file__)
+        # for filename in glob(join(curdir, 'images', '*')):
+
+        filename = 'resources/Capture 3 - Position 6_XY1543355356_Z0_T000_C0.png'
+        try:
+            # load the image
+            picture = Picture(source=filename)  # rotation=randint(-30, 30))
+            # add to the main field
+            root.add_widget(picture)
+        except Exception as e:
+            Logger.exception('Pictures: Unable to load <%s>' % filename)
 
     def on_pause(self):
         return True
 
 
+class LoadDialog(FloatLayout):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+class Root(FloatLayout):
+    loadfile = ObjectProperty(None)
+    savefile = ObjectProperty(None)
+    clearline = ObjectProperty(None)
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def show_load(self):
+        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Load file", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def load(self, path, filename):
+        with open(os.path.join(path, filename[0])) as stream:
+            self.text_input.text = stream.read()
+        self.canvas.clear()
+        self.dismiss_popup()
+
+    def clear_line(self):
+        self.canvas.clear()
+
+class Editor(App):
+
+    def build(self):
+
+        # the root is created in pictures.kv
+        root = self.root
+
+        # get any files into images directory
+        # curdir = dirname(__file__)
+        # for filename in glob(join(curdir, 'images', '*')):
+
+        filename = 'resources/Capture 3 - Position 6_XY1543355356_Z0_T000_C0.png'
+        try:
+            # load the image
+            picture = Picture(source=filename)  # rotation=randint(-30, 30))
+            # add to the main field
+            root.add_widget(picture)
+        except Exception as e:
+            Logger.exception('Pictures: Unable to load <%s>' % filename)
+
+    def on_pause(self):
+        return True
+
+
+Factory.register('Root', cls=Root)
+Factory.register('LoadDialog', cls=LoadDialog)
+
 if __name__ == '__main__':
-    PicturesApp().run()
+    Editor().run()
+    #PicturesApp().run()
