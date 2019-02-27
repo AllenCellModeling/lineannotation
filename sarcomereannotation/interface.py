1c96eb78b8d2b4191582c35ccd8bb46852166b26
Jamie Sherman <jamie.sherman@gmail.com>
2019-02-27 15:54:14 -0800

    image loading and y scrolling work, need to add point tracking

--- sarcomereannotation/interface.py
+++ sarcomereannotation/interface.py
@@ -33,15 +33,43 @@ from kivy.app import App
 from kivy.logger import Logger
 from kivy.uix.widget import Widget
 from kivy.uix.scatter import Scatter
+from kivy.uix.image import Image
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.slider import Slider
+from kivy.uix.recycleview import RecycleView
 from kivy.properties import StringProperty
 from kivy.uix.floatlayout import FloatLayout
 from kivy.factory import Factory
 from kivy.properties import ObjectProperty, NumericProperty
 from kivy.uix.popup import Popup
-
+from kivy.core.window import Window
+import json
 import os
 
-class Picture(Scatter):
+
+class SarcomereLines(object):
+    def __init__(self):
+        self.lines = []
+        self.add_line()
+
+    def add_line(self):
+        self.lines.append([])
+
+    def add_point(self, point):
+        self.lines[-1].append(point.pos)
+        print(str(point.pos))
+
+    def remove_point(self):
+        self.lines[-1].pop()
+
+    def write_file(self, fname):
+        with open(fname, "w") as fp:
+            json.dump(self.lines, fp)
+        fp.closed
+
+
+
+class Picture(Image):
     '''Picture is the class that will show the image with a white border and a
     shadow. They are nothing here because almost everything is inside the
     picture.kv. Check the rule named <Picture> inside the file, and you'll see
@@ -53,6 +81,14 @@ class Picture(Scatter):
     do_scale = True
     source = StringProperty(None)
 
+    def __init__(self, **kwargs):
+        super(Picture, self).__init__(**kwargs)
+        self.txt_name = kwargs["source"] + ".txt"
+        self.keep_points = SarcomereLines()
+
+    def on_touch_down(self, touch):
+        self.keep_points.add_point(touch)
+        self.keep_points.write_file(self.txt_name)
 
 # class PicturesApp(App):
 #
@@ -93,21 +129,24 @@ class Root(FloatLayout):
     loadfile = ObjectProperty(None)
     savefile = ObjectProperty(None)
     clearline = ObjectProperty(None)
+    Window.size = (1024, 1074)
 
     def add_picture(self, path):
-        filename = 'resources/Capture 3 - Position 6_XY1543355356_Z0_T000_C0.png'
+        filename = path
+        #filename = 'resources/Capture 3 - Position 6_XY1543355356_Z0_T000_C0.png'
         try:
             # load the image
-            lpicture = Picture(source=filename,
-                               pos_hint={'top': 0.98, 'right': 0.98},
+            sv = ScrollView(size_hint=(0.9, 0.9), pos_hint={'top': 0.975, 'right': 0.95})  #  ScrollView(size=(1024, 1024))
+            lpicture = Picture(source=filename, # pos_hint={'top': 0.98, 'right': 0.98},
                                )  # rotation=randint(-30, 30))
             # add to the main field
-            self.add_widget(lpicture)
+            sv.add_widget(lpicture)
+            self.add_widget(sv)      # lpicture)
 
         except Exception as e:
             Logger.exception('Pictures: Unable to load <%s>' % filename)
             exit(-1)
-        self.picture = lpicture
+        self.picture = sv
 
     def on_pause(self):
         return True
