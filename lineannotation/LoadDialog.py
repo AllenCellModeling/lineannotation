694c527fb66954ca26373707904be20a3e95a0ec
Jamie Sherman <jamie.sherman@gmail.com>
2019-03-06 16:32:00 -0800

    updated program to use user home directory as the default folder

--- lineannotation/LoadDialog.py
+++ lineannotation/LoadDialog.py
@@ -3,8 +3,10 @@ from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 from kivy.uix.floatlayout import FloatLayout
 from .LoadDialog_View import LOADDIALOG_VIEW
+import pathlib
+from os import path
 
-Builder.load_string(LOADDIALOG_VIEW.format(IMAGE_FOLDER))
+Builder.load_string(LOADDIALOG_VIEW.format(pathlib.Path(path.expanduser("~")).resolve()))
 
 
 class LoadDialog(FloatLayout):
